7f65fcd1aefcef5ff8ea74befaaa0b58b56236cb
sugadeva <idn4914@gmail.com>
2025-07-27 09:22:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  API_KEY: "PTLC_xxxxxx", // Ganti dengan API Key kamu
-  PANEL_URL: "https://panel.kamu.my.id", // Ganti dengan URL panel kamu
-  ADMIN_PASSWORD: "adminbanget123" // Ganti dengan password admin kamu
+  API_KEY: "ptla_fZgQdhr1ba0vYJT9zN3qeo0TdIEwGZugf0gjUPQWZIs", // Ganti dengan API Key kamu
+  PANEL_URL: "https://bakzxdoyzptown.cloud-hosting.biz.id", // Ganti dengan URL panel kamu
+  ADMIN_PASSWORD: "anjay" // Ganti dengan password admin kamu
 };
